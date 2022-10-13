ae42248aa2e3dcd23ea80a822c5feeda3ea50096
Danilus120 <tomaszdanilczuk@gmail.com>
2022-10-13 22:15:24 +0200

    add validationObject

--- constants/validationSchema.ts
+++ constants/validationSchema.ts
@@ -2,6 +2,22 @@ import * as yup from "yup";
 import YupPassword from "yup-password";
 YupPassword(yup);
 
+const validationObject = {
+  password: yup
+    .string()
+    .min(
+      8,
+      "Password must contain 8 or more characters with at least one of each: uppercase, lowercase, number and special"
+    )
+    .minLowercase(1, "Password must contain at least 1 lower case letter")
+    .minUppercase(1, "Password must contain at least 1 upper case letter")
+    .minNumbers(1, "Password must contain at least 1 number")
+    .minSymbols(1, "Password must contain at least 1 special character")
+    .required(),
+  email: yup.string().email().required(),
+  displayName: yup.string().required(),
+};
+
 const settingsSchema = yup
   .object({
     currency: yup.string().required(),
@@ -11,42 +27,22 @@ const settingsSchema = yup
 
 const loginFormSchema = yup
   .object({
-    email: yup.string().email().required(),
-    password: yup
-      .string()
-      .min(
-        8,
-        "Password must contain 8 or more characters with at least one of each: uppercase, lowercase, number and special"
-      )
-      .minLowercase(1, "Password must contain at least 1 lower case letter")
-      .minUppercase(1, "Password must contain at least 1 upper case letter")
-      .minNumbers(1, "Password must contain at least 1 number")
-      .minSymbols(1, "Password must contain at least 1 special character")
-      .required(),
+    email: validationObject.email,
+    password: validationObject.password,
   })
   .required();
 
 const registerFormSchema = yup
   .object({
-    displayName: yup.string().required(),
-    email: yup.string().email().required(),
-    password: yup
-      .string()
-      .min(
-        8,
-        "Password must contain 8 or more characters with at least one of each: uppercase, lowercase, number and special"
-      )
-      .minLowercase(1, "Password must contain at least 1 lower case letter")
-      .minUppercase(1, "Password must contain at least 1 upper case letter")
-      .minNumbers(1, "Password must contain at least 1 number")
-      .minSymbols(1, "Password must contain at least 1 special character")
-      .required(),
+    displayName: validationObject.displayName,
+    email: validationObject.email,
+    password: validationObject.password,
   })
   .required();
 
 const forgotPasswordSchema = yup
   .object({
-    email: yup.string().email().required(),
+    email: validationObject.email,
   })
   .required();
 

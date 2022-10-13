fb5f5b8ee01857e5c39d4430094b18585da47348
Danilus120 <tomaszdanilczuk@gmail.com>
2022-10-13 11:52:09 +0200

    Created Form with react-hook-form

---
+++ components/Molecules/Form/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+interface FormI {
+  defaultValues?: Record<string, any>;
+  children: React.ReactNode;
+  onSubmit: () => void;
+}
+
+export default function Form({ defaultValues, children, onSubmit }: FormI) {
+  const { handleSubmit, register } = useForm({ defaultValues });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {Array.isArray(children)
+        ? children.map((child) => {
+            return child.props.name
+              ? React.createElement(child.type, {
+                  ...{
+                    ...child.props,
+                    register,
+                    key: child.props.name,
+                  },
+                })
+              : child;
+          })
+        : children}
+    </form>
+  );
+}

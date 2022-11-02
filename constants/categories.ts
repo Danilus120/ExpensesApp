0fc5c752713c8421b0016e00c5249444a5bed59b
Danilus120 <tomaszdanilczuk@gmail.com>
2022-11-02 18:39:34 +0100

    remake list of categories

--- constants/categories.ts
+++ constants/categories.ts
@@ -1,6 +1,12 @@
 const categories = [
+  { label: "Car and transport", value: "car-and-transport" },
+  { label: "Clothing and footwear", value: "clothing-and-footwear" },
+  { label: "Entertainment and travel", value: "entertainment-and-travel" },
+  { label: "Finance", value: "finance" },
   { label: "Food", value: "food" },
-  { label: "Game", value: "game" },
+  { label: "Health and beauty", value: "health-and-beauty" },
+  { label: "Home and bills", value: "home-and-bills" },
+  { label: "Others", value: "others" },
 ];
 
 export { categories };

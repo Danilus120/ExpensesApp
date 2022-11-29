a44c294eebc8bcd096df86a339c83fb69b529e71
Danilus120 <tomaszdanilczuk@gmail.com>
2022-11-29 15:28:19 +0100

    add visual side of goals

--- components/Organisms/DashboardGoals/index.tsx
+++ components/Organisms/DashboardGoals/index.tsx
@@ -1,38 +1,71 @@
 import { useData } from "@/context/UserDataContext";
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { calculateAvgSavingsFromLastMonths } from "utils/goals/utils";
+
+import styles from "./styles.module.scss";
 
 function DashboardGoals() {
   const { userData } = useData();
-  const [valueToSave, setValueToSave] = useState(0);
-  const [monthsTimeRange, setMonthsTimeRange] = useState(2);
   const [avgSavings, setAvgSavings] = useState(0);
+  const [goalState, setGoalState] = useState({
+    valueToSave: 0,
+    monthsTimeRange: 1,
+  });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValueToSave(Number(e.target.value));
-  };
-
-  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setMonthsTimeRange(Number(e.target.value));
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setGoalState((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: Number(e.target.value),
+      };
+    });
   };
 
   useEffect(() => {
     setAvgSavings(
-      calculateAvgSavingsFromLastMonths(valueToSave, userData, monthsTimeRange)
+      calculateAvgSavingsFromLastMonths(
+        goalState.valueToSave,
+        userData,
+        goalState.monthsTimeRange
+      )
     );
-  }, [monthsTimeRange, userData, valueToSave]);
+  }, [goalState, userData]);
 
   return (
-    <>
-      <input type="number" onChange={(e) => handleInputChange(e)} />
-      <select name="" id="" onChange={(e) => handleSelectChange(e)}>
-        <option value="1">1 month</option>
-        <option value="3">3 months</option>
-        <option value="6">6 months</option>
-        <option value="12">12 months</option>
-      </select>
-      {avgSavings} months of savings
-    </>
+    <div className={`${styles["goals"]}`}>
+      <h3>Calculate time to achieve your goal</h3>
+      <div className={`${styles["calculator"]}`}>
+        <div className={`${styles["container"]}`}>
+          <label htmlFor="valueToSave">Value of your goal</label>
+          <input
+            type="number"
+            name="valueToSave"
+            id="valueToSave"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className={`${styles["container"]}`}>
+          <label htmlFor="monthsTimeRange">
+            From how many months will be calculate avg.
+          </label>
+          <select
+            name="monthsTimeRange"
+            id="monthsTimeRange"
+            onChange={(e) => handleChange(e)}
+          >
+            <option value="1">1 month</option>
+            <option value="3">3 months</option>
+            <option value="6">6 months</option>
+            <option value="12">12 months</option>
+          </select>
+        </div>
+      </div>
+      <div className={`${styles["summary"]}`}>
+        You will achieve your goal by {avgSavings} months
+      </div>
+    </div>
   );
 }
 

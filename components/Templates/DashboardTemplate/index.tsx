0a43ca0edd14d4de9c5768c76f6b3780d14951ed
Danilus120 <tomaszdanilczuk@gmail.com>
2022-12-05 20:34:14 +0100

    FIX: Redirect not logged users to /login page while opening /dashboard

--- components/Templates/DashboardTemplate/index.tsx
+++ components/Templates/DashboardTemplate/index.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import ContentTemplate from "./components/ContentTemplate/ContentTemplate";
 
 import { UserDataContextProvider } from "@/context/UserDataContext";
+import { useAuth } from "@/context/auth/AuthContext";
+import Router from "next/router";
 
 interface DashboardTemplateProps {
   children: React.ReactNode;
@@ -16,6 +18,14 @@ export default function DashboardTemplate({
   children,
   metaOptions,
 }: DashboardTemplateProps) {
+  const { user } = useAuth();
+
+  // If user is not logged in, change url to /login
+  if (!user) {
+    Router.push("/login");
+    return;
+  }
+
   return (
     <>
       <Head>
